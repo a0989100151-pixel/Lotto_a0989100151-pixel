1a9912e366fe542ca8298af70077027dcfef8981
a0989100151-pixel <a0989100151@gmail.com>
2025-09-22 15:31:54 +0800

    第七次commit (螢幕觸控)(黃福恩)

--- app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
@@ -25,7 +25,6 @@ import tw.edu.pu.csim.tcyang.lotto.ui.theme.LottoTheme
 import androidx.compose.runtime.setValue // 引入 setValue
 import androidx.compose.ui.platform.LocalContext
 
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,9 +32,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LottoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Play(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Play(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -44,32 +41,39 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Play(modifier: Modifier = Modifier) {
-    //var lucky = (1..100).random()
     var lucky by remember {
-        mutableStateOf((1..100).random())
+        mutableStateOf((1..100).random())  // 隨機產生 1 到 100 之間的數字
     }
 
     val context = LocalContext.current // 取得當前 Context
 
-    Column (modifier = modifier
-        .fillMaxSize()
-        .clickable {
-            Toast.makeText(context, "螢幕觸控(楊子青)", Toast.LENGTH_SHORT).show()
-        },
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-        ){
+    Column(
+        modifier = modifier
+            .fillMaxSize()  // 讓 Column 填滿整個螢幕
+            .clickable {  // 設定觸摸事件，點擊全螢幕觸發 Toast
+                Toast.makeText(context, "螢幕觸控(黃福恩)", Toast.LENGTH_SHORT).show()
+            },
+        horizontalAlignment = Alignment.CenterHorizontally,  // 文字和元件水平置中
+        verticalArrangement = Arrangement.Center  // 元件垂直置中
+    ) {
+        // 顯示樂透數字
         Text(
             text = "樂透數字(1-100)為 $lucky"
         )
 
+        // 重新產生樂透碼的按鈕
         Button(
-            onClick = { lucky = (1..100).random() }
+            onClick = { lucky = (1..100).random() }  // 點擊按鈕後產生新數字
         ) {
             Text("重新產生樂透碼")
         }
     }
+}
 
-
-
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LottoTheme {
+        Play()
+    }
 }
